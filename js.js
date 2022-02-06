45156be362bda0d0b8f0583ba1be3affd96c52b9
Hmidoukourouk <marcos.pierre@orange.fr>
2022-02-07 00:39:24 +0100

    waw

--- js.js
+++ js.js
@@ -3,28 +3,37 @@ var finalResult
 var length;
 var firstCharacter;
 
-function test(){
+function Conversion() {
     textval = document.getElementById("textArea").value;
     length = textval.length;
-    finalResult = "";
-    firstCharacter = true;
 
-    if (length == 0){
+    if (length == 0) {
         alert("fréro rentre un truc la");
-    }else {
+    } else {
+        finalResult = "";
+        firstCharacter = true;
+        textval = textval.replaceAll("\n", "\nENTER\nSTRING├ÙM");
+        length = textval.length;
         for (let index = 0; index < length; index++) {
-            if (textval.charAt(index)==" "){
-                finalResult = finalResult + "<br> SPACE <br>";
+            if (textval.charAt(index) == " ") {
+                finalResult = finalResult + "\nSPACE\n";
                 firstCharacter = true;
-            }else{
-                if (firstCharacter == true){
-                    finalResult = finalResult + "STRING ";
+            } else {
+                if (firstCharacter == true) {
+                    finalResult = finalResult + "STRING├ÙM";
                     firstCharacter = false;
                 }
-                finalResult = finalResult + textval.charAt(index);
+                finalResult = (finalResult + textval.charAt(index));
             }
         }
     }
-    finalResult = finalResult.replaceAll("\n", "<br> ENTER <br> STRING ");
+    finalResult = finalResult.replaceAll("├ÙM", " ");
     document.getElementById("result").innerHTML = finalResult;
+}
+
+function Copie() {
+    finalResult = document.getElementById("result");
+    finalResult.select();
+/* Copy the text inside the text field */
+    document.execCommand("copy");
 }
